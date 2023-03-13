97510f210a5c9c22b37c5f851f389e39efa50649
shoichi1023 <so_1117@outlook.jp>
2023-03-13 21:52:09 +0900

    refactor: :refactoring: refactor

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use anyhow::anyhow;
-use image::{self, imageops::FilterType::Lanczos3, DynamicImage, RgbImage};
-use mozjpeg::{ColorSpace, Compress, Decompress, Marker, ScanMode, ALL_MARKERS};
-use std::fs;
-
 use std::env;
-use std::io::{self, Read, Write};
+use std::fs::{self, File};
+use std::io::{self, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
 
+use image::imageops::FilterType;
+use image::{self};
+use mozjpeg::{ColorSpace, Compress, ScanMode};
 use rayon::prelude::*;
 
-const TARGET_SIZE: u32 = 1280;
+const TARGET_SIZE: usize = 1280;
 
 fn main() {
     let target_dir = match env::args().nth(1) {
-        Some(v) => PathBuf::from(v).parent().unwrap().join("compressed"),
+        Some(v) => PathBuf::from(v).parent().unwrap().join(""),
         None => return,
     };
     if !target_dir.exists() {
@@ -30,103 +29,91 @@ fn main() {
         .par_iter()
         .for_each(|path| match process(&path, &target_dir) {
             Ok(file_name) => println!("{} is compressed.", file_name),
-            Err(err) => println!("FAILED due to {}", err),
+            Err((file_name, err)) => println!("{} FAILED due to {}", file_name, err),
         });
     pause();
 }
 
-fn process(path: &Path, target_dir: &Path) -> anyhow::Result<String> {
+fn process(path: &Path, target_dir: &Path) -> Result<String, (String, String)> {
     let file_name = path.file_name().unwrap().to_string_lossy().to_string();
 
-    let raw_data = fs::read(path)?;
-    let decomp = Decompress::with_markers(ALL_MARKERS).from_mem(&raw_data)?;
+    let (resized_img_data, target_width, target_height) = match resize(path) {
+        Ok(v) => v,
+        Err(e) => return Err((file_name, e.to_string())),
+    };
 
-    // markers の中に Exif 情報がある
-    let markers: Vec<(Marker, Vec<u8>)> = decomp
-        .markers()
-        .into_iter()
-        .map(|m| (m.marker, m.data.to_owned()))
-        .collect();
+    let compressed_img_data = match compress(resized_img_data, target_width, target_height) {
+        Ok(v) => v,
+        Err(e) => return Err((file_name, e.to_string())),
+    };
 
-    // RGB 形式でデコード開始
-    let mut decomp_started = decomp.rgb()?;
+    let target_file = target_dir.join("resized_".to_string() + &file_name);
+    let mut file =
+        BufWriter::new(File::create(target_file).map_err(|e| (file_name.clone(), e.to_string()))?);
+    file.write_all(&compressed_img_data)
+        .map_err(|e| (file_name.clone(), e.to_string()))?;
 
-    // 幅・高さ取得
-    let width = decomp_started.width();
-    let height = decomp_started.height();
+    Ok(file_name)
+}
 
-    // デコードされたデータの取得
-    let data = decomp_started
-        .read_scanlines::<[u8; 3]>()
-        .ok_or(anyhow!("read_scanlines error"))?
-        .iter()
-        .flatten()
-        .cloned()
-        .collect();
-
-    // デコードの終了処理
-    decomp_started.finish_decompress();
-
-    // image crate の DynamicImage に変換
-    let image_buffer =
-        RgbImage::from_raw(width as u32, height as u32, data).ok_or(anyhow!("from_raw error"))?;
-    let img = DynamicImage::ImageRgb8(image_buffer);
-
-    // リサイズとシャープ処理
-    // 1) resize はアスペクトレシオを保持する
-    // 2) unshrpen の一つ目の引数はどの程度ぼかしを入れるか（0.5~5.0 ぐらい？）
-    // 　　二つ目の引数はしきい値（1~10 ぐらい？）
-    // 　　どのぐらいの数値が良いかは画像によって変わる
-    let img = img
-        .resize(
-            TARGET_SIZE,
-            TARGET_SIZE * img.height() / img.width(),
-            Lanczos3,
-        )
-        .unsharpen(0.5, 10);
-
-    // リサイズ後の幅・高さ取得
+fn resize(path: &Path) -> Result<(Vec<u8>, usize, usize), String> {
+    let img = image::open(path).map_err(|e| e.to_string())?;
     let width = img.width() as usize;
     let height = img.height() as usize;
 
-    // 変換後の RGB データ取得
-    let data = img.to_rgb8().to_vec();
+    if width > TARGET_SIZE || height > TARGET_SIZE {
+        let (target_width, target_height) = if width > height {
+            let ratio: f32 = TARGET_SIZE as f32 / width as f32;
+            (TARGET_SIZE, (height as f32 * ratio) as usize)
+        } else {
+            let ratio: f32 = TARGET_SIZE as f32 / height as f32;
+            ((width as f32 * ratio) as usize, TARGET_SIZE)
+        };
+        let resized_img = img.resize(
+            target_width as u32,
+            target_height as u32,
+            FilterType::Lanczos3,
+        );
+        Ok((
+            resized_img.to_rgb8().to_vec(),
+            resized_img.width() as usize,
+            resized_img.height() as usize,
+        ))
+    } else {
+        Ok((img.to_rgb8().to_vec(), width, height))
+    }
+}
 
-    // mozjpeg での圧縮処理
+fn compress(
+    resized_img_data: Vec<u8>,
+    target_width: usize,
+    target_height: usize,
+) -> Result<Vec<u8>, String> {
     let mut comp = Compress::new(ColorSpace::JCS_RGB);
     comp.set_scan_optimization_mode(ScanMode::AllComponentsTogether);
     comp.set_quality(70.0);
-    comp.set_size(width, height);
+
+    comp.set_size(target_width, target_height);
+
     comp.set_mem_dest();
     comp.start_compress();
 
-    // Exif 情報を書き込む
-    markers.into_iter().for_each(|m| {
-        comp.write_marker(m.0, &m.1);
-    });
-
-    // RGB データを書き込む
     let mut line = 0;
     loop {
-        if line > height - 1 {
+        if line > target_height - 1 {
             break;
         }
-        let buf = unsafe { data.get_unchecked(line * width * 3..(line + 1) * width * 3) };
-        comp.write_scanlines(buf);
+        comp.write_scanlines(
+            &resized_img_data[line * target_width * 3..(line + 1) * target_width * 3],
+        );
         line += 1;
     }
-
-    // 圧縮の終了処理
     comp.finish_compress();
 
-    // ファイルに保存
-    let buf = comp.data_to_vec().map_err(|e| anyhow!("{:?}", e))?;
-    fs::write(
-        format!("{}/{}_resized.jpg", target_dir.to_string_lossy(), file_name),
-        &buf,
-    )?;
-
-    Ok(file_name)
+    let compressed = comp
+        .data_to_vec()
+        .map_err(|_| "data_to_vec failed".to_string())?;
+    Ok(compressed)
 }
 
 fn pause() {
